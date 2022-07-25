7b963b2e2751c75aec05809783d12787899dd501
liliang <ll917274996@live.com>
2022-07-25 17:39:55 +0800

    优化

--- components/video/Banner.vue
+++ components/video/Banner.vue
@@ -1,29 +1,22 @@
 <template>
-  <div :class="cls">
-    <img :class="`${cls}_bg`" src="~~/assets/video/banner1.png" alt="banner1">
-    <h2 :class="`${cls}_title`" v-html="$t.value.videoPage.bannerTitle"></h2>
-  </div>
+  <VideoCard box-class="video-banner_box" :title="$t.value.videoPage.bannerTitle">
+    <template #bg>
+      <img src="~~/assets/video/banner1.png" alt="">
+    </template>
+  </VideoCard>
 </template>
 
-<script setup>
-const cls = 'video-banner'
-</script>
-
 <style lang="less">
-.video-banner {
-  position: relative;
-  &_bg {
-    display: block;
-    width: 100%;
-  }
-  &_title {
-    position: absolute;
-    top: 35.8%;
-    left: 25.5%;
+.video-banner_box {
+  left: 25.5%;
+  h2 {
     font-size: 2rem;
-    font-weight: normal;
     color: #fff;
-    line-height: 1.3;
+  }
+  @media screen and (max-width: 600px) {
+    h2 {
+      font-size: 1.5rem;
+    }
   }
 }
 </style>
--- components/video/Card.vue
+++ components/video/Card.vue
@@ -8,7 +8,7 @@
         <h2 :class="`${cls}_title`" v-html="title"></h2>
         <h3 v-if="subTitle" :class="`${cls}_subtitle`">{{ subTitle }}</h3>
       </div>
-      <div :class="`${cls}_content`">
+      <div v-if="$slots.default || desc" :class="`${cls}_content`">
         <slot>
           <p :class="`${cls}_desc`" v-html="desc"></p>
         </slot>
@@ -40,8 +40,8 @@ const cls = 'video-card'
     top: 50%;
     transform: translateY(-50%);
   }
-  &_titlebox {
-    margin-bottom: 2rem;
+  &_content {
+    margin-top: 2rem;
   }
   &_title {
     font-size: 3.2rem;
