20e874a1953ce921757062a0abeba740ce6749f0
liliang <ll917274996@live.com>
2022-08-01 10:02:07 +0800

    优化

--- components/home/Partner.vue
+++ components/home/Partner.vue
@@ -1,11 +1,8 @@
 <template>
   <div :class="cls">
     <h2 :class="`${cls}_title`">{{ title }}</h2>
-    <ul :class="`${cls}_list`">
-      <li v-for="i in 10" :key="i"></li>
-    </ul>
-    <div :class="`${cls}_btn-group`">
-      <a :class="`${cls}_btn-more`">MORE +</a>
+    <div :class="`${cls}_list`">
+      <img v-for="_ in parters" :key="_.src" :src="_.src" alt="">
     </div>
   </div>
 </template>
@@ -15,37 +12,19 @@ defineProps({
   title: String
 })
 const cls = 'home-parter'
+const parters = ref([])
 </script>
 
 <style lang="less">
 .home-parter {
+  text-align: center;
   &_title {
     font-size: 2rem;
-    text-align: center;
     margin-bottom: 2rem;
   }
   &_list {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    li {
-      width: 200px;
-      height: 50px;
-      border: 1px solid #707070;
-      margin: 0 1.5rem 2rem;
-    }
-  }
-  &_btn-group {
-    text-align: center;
-  }
-  &_btn-more {
-    display: inline-block;
-    border: 1px solid #707070;
-    padding: .3rem;
-    border-radius: 6px;
-    &:hover {
-      border-color: currentColor;
+    img {
+      margin: 0 1rem 2rem;
     }
   }
 }
